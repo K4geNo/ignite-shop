b502d9aa580a585f7a60590e1322eac623697bef
K4geNo <micaraujo71@gmail.com>
2022-12-08 09:36:40 -0300

    Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import { styled } from "@/styles"
+
+const Button = styled("button", {
+    backgroundColor: "#ffeadb",
+    cursor: "pointer",
+    borderRadius: 4,
+    border: "none",
+    padding: "4px 8px",
+
+    span: {
+        color: "#ff0000",
+    },
+
+    "&:hover": {
+        backgroundColor: "#ffeadb",
+        filter: "brightness(0.9)",
+    },
+})
+
 export default function Home() {
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <Button>
+            <span>Click me</span>
+        </Button>
     )
 }
