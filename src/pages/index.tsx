d2839030d158ef5483861979936c583515f9865a
K4geNo <micaraujo71@gmail.com>
2022-12-08 14:59:31 -0300

    Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,30 @@
-import { styled } from "@/styles"
+import { HomeContainer, Product } from "@/styles/pages/home"
 
-const Button = styled("button", {
-    backgroundColor: "#ffeadb",
-    cursor: "pointer",
-    borderRadius: 4,
-    border: "none",
-    padding: "4px 8px",
-
-    span: {
-        color: "#ff0000",
-    },
-
-    "&:hover": {
-        backgroundColor: "#ffeadb",
-        filter: "brightness(0.9)",
-    },
-})
+import Image from "next/image"
+import camiseta1 from "@/assets/camisetas/1.png"
+import camiseta2 from "@/assets/camisetas/2.png"
+import camiseta3 from "@/assets/camisetas/3.png"
 
 export default function Home() {
     return (
-        <Button>
-            <span>Click me</span>
-        </Button>
+        <HomeContainer>
+            <Product>
+                <Image src={camiseta1} width={520} height={480} alt="" />
+
+                <footer>
+                    <strong>Camiseta X</strong>
+                    <span>R$ 59,90</span>
+                </footer>
+            </Product>
+
+            <Product>
+                <Image src={camiseta2} width={520} height={480} alt="" />
+
+                <footer>
+                    <strong>Camiseta Y</strong>
+                    <span>R$ 59,90</span>
+                </footer>
+            </Product>
+        </HomeContainer>
     )
 }
