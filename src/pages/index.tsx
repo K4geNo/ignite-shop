7066624522ff992ae7c32331cdc3eeb9804fee6b
K4geNo <micaraujo71@gmail.com>
2022-12-12 16:57:46 -0300

    SSG com parâmetro dinâmico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { HomeContainer, Product } from "@/styles/pages/home"
 
 import { GetStaticProps } from "next"
 import Image from "next/image"
+import Link from "next/link"
 import Stripe from "stripe"
 import { stripe } from "@/lib/stripe"
 import { useKeenSlider } from "keen-slider/react"
@@ -13,7 +14,7 @@ interface HomeProps {
         id: string
         name: string
         imageUrl: string
-        price: number
+        price: string
     }[]
 }
 
@@ -28,19 +29,26 @@ export default function Home({ products }: HomeProps) {
     return (
         <HomeContainer ref={sliderRef} className="keen-slider">
             {products.map((product) => (
-                <Product className="keen-slider__slide" key={product.id}>
-                    <Image
-                        src={product.imageUrl}
-                        width={520}
-                        height={480}
-                        alt=""
-                    />
+                <Link
+                    href={`/product/${product.id}`}
+                    key={product.id}
+                    legacyBehavior
+                    prefetch={false}
+                >
+                    <Product className="keen-slider__slide">
+                        <Image
+                            src={product.imageUrl}
+                            width={520}
+                            height={480}
+                            alt=""
+                        />
 
-                    <footer>
-                        <strong>{product.name}</strong>
-                        <span>{product.price}</span>
-                    </footer>
-                </Product>
+                        <footer>
+                            <strong>{product.name}</strong>
+                            <span>{product.price}</span>
+                        </footer>
+                    </Product>
+                </Link>
             ))}
         </HomeContainer>
     )
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,83 @@
-export default function Product() {
+import { GetStaticPaths, GetStaticProps } from "next"
+import {
+    ImageContainer,
+    ProductContainer,
+    ProductDetails,
+} from "@/styles/pages/product"
+
+import Image from "next/image"
+import Stripe from "stripe"
+import { stripe } from "@/lib/stripe"
+import { useRouter } from "next/router"
+
+interface ProductProps {
+    product: {
+        id: string
+        name: string
+        imageUrl: string
+        price: string
+        description: string
+    }
+}
+
+export default function Product({ product }: ProductProps) {
     return (
-        <div>
-            <h1>Product</h1>
-        </div>
+        <ProductContainer>
+            <ImageContainer>
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>{product.name}</h1>
+                <span>{product.price}</span>
+
+                <p>{product.description}</p>
+
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
     )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+    params,
+}) => {
+    if (!params) return { notFound: true }
+
+    const productId = params.id
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ["default_price"],
+    })
+
+    const price = product.default_price as Stripe.Price
+
+    return {
+        props: {
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0],
+                price: new Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                }).format(price.unit_amount! / 100),
+                description: product.description,
+            },
+        },
+        revalidate: 60 * 60 * 1, // 1 hours
+    }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const products = await stripe.products.list()
+
+    const paths = products.data.map((product) => ({
+        params: { id: product.id },
+    }))
+
+    return {
+        paths,
+        fallback: "blocking",
+    }
 }
